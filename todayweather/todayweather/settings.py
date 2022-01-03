8c6bcdb4b1520a688e066a25c56498ae6aeb0d43
choiseyeon <choi20014830@gmail.com>
2022-01-03 21:35:27 +0900

    database 연결

--- todayweather/todayweather/settings.py
+++ todayweather/todayweather/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'todayweather.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',    
+        'NAME': 'todaywheather',                
+        'USER': 'root',                          
+        'PASSWORD': 'Chltpdus6738*',                  
+        'HOST': 'localhost',                     
+        'PORT': '3306',                          
     }
 }
 
